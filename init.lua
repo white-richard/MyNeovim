df071204847dc88a5996713475c283c6d24c181a
white-richard <rich.white285@yahoo.com>
2025-08-26 22:27:23 -0700

    Added file undo persistance

--- init.lua
+++ init.lua
@@ -5,3 +5,8 @@ require('options')
 require('colorscheme')
 require('lsp')
 require('keymaps')
+
+-- Enable persistent undo
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undo"
+
