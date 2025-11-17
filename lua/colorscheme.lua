21793f2de6958da4c36fb94ab61fdae4ff457f7e
white-richard <rich.white285@yahoo.com>
2025-11-17 00:03:49 -0800

    kanagawa over rosepine

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = 'rose-pine'
+local colorscheme = 'kanagawa'
 local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not is_ok then
     vim.notify('colorscheme ' .. colorscheme ' not found')
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "rose-pine", -- or "tokyonight", "dracula", "catppuccin", "auto"
+        theme = "kanagawa", -- or "tokyonight", "dracula", "catppuccin", "auto", "kanagawa"
       },
     })
   end,
