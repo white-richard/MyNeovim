22b088b0770f30eeed13f5990ff3cceb7c650e1a
white-richard <rich.white285@yahoo.com>
2025-11-16 17:51:55 -0800

    fix null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,20 +1,29 @@
 return {
     "nvimtools/none-ls.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "williamboman/mason.nvim",
+        "jay-babu/mason-null-ls.nvim",
+    },
     config = function()
+        require("mason").setup()
+
+        -- Will auto-install Ruff if it's listed in your none-ls sources
+        require("mason-null-ls").setup({
+            ensure_installed = { "ruff" }, -- THIS installs the Ruff binary
+            automatic_installation = true,
+        })
+
         local null_ls = require("null-ls")
+
         null_ls.setup({
             sources = {
-                -- examples:
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.diagnostics.ruff,
+
                 null_ls.builtins.formatting.black,
             },
         })
-        vim.keymap.set({ "n", "v" }, "<leader>wf", function()
-            vim.lsp.buf.format({ async = true })
-        end, { desc = "Format buffer or selection" })
     end,
 }
