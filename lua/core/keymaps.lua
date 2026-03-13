b2e3332e1dc599f203a763ce111112f88df65cc1
white-richard <rich.white285@yahoo.com>
2026-03-12 20:20:38 -0700

    remove some annoying vim settings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,8 +2,6 @@
 vim.keymap.set('n', '<Space>y', '"+y', { desc = 'Copy to system clipboard' })
 vim.keymap.set('v', '<Space>y', '"+y', { desc = 'Copy selected text to system clipboard' })
 -- Delete without copying into the register
-vim.keymap.set({ 'n', 'v' }, 'd', '"_d')
-vim.keymap.set({ 'n', 'v' }, 'c', '"_c')
 vim.keymap.set({ 'n', 'v' }, 'x', '"_x')
 
 -- Set leader key
